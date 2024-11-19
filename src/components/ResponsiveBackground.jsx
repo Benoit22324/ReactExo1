9ef56dae8566b59ebf83d5669ca98b488e5c30d1
Benoit22324 <sunbenoit384@gmail.com>
2024-11-19 11:33:36 +0100

    Ajout du ResponsiveBackground

--- src/components/ResponsiveBackground.jsx
+++ src/components/ResponsiveBackground.jsx
@@ -2,13 +2,15 @@ import { useEffect, useState } from "react"
 
 export const ResponsiveBackground = () => {
     const [windowWidth, setWindowWidth] = useState(0);
+    const [actualColor, setActualColor] = useState("");
 
     const handleResponsive = () => {
         setWindowWidth(window.innerWidth);
+        setActualColor(window.innerWidth >= 800 ? "#ADD8E6" : "#FFC0CB");
     }
 
     const customResponsiveStyle = {
-        backgroundColor: windowWidth >= 800 ? "#ADD8E6" : "#FFC0CB",
+        backgroundColor: actualColor,
         width: "fit-content",
         padding: "5px",
         borderRadius: "5px"
@@ -17,16 +19,12 @@ export const ResponsiveBackground = () => {
     useEffect(() => {
         handleResponsive();
 
-        window.addEventListener('resize', () => {
-            handleResponsive();
-        })
+        // window.addEventListener('resize', handleResponsive);
 
-        return () => {
-            window.removeEventListener('resize', () => {
-                handleResponsive()
-            });
-        }
-    }, [])
+        // return () => {
+        //     window.removeEventListener('resize', handleResponsive);
+        // }
+    }, []);
 
     return <>
         <p style={customResponsiveStyle}>La taille actuelle est de {windowWidth}px</p>
