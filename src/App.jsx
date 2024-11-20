a1b94c4cf0247c4bb07d7be2384afba18f33a0a4
Benoit22324 <sunbenoit384@gmail.com>
2024-11-20 11:41:50 +0100

    Nettoyage + Ajout du store.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./assets/css/App.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { Navbar } from "./components/Navbar"
-import { Home } from "./components/Home"
-import { Users } from "./components/Users"
+import { Navbar } from "./components/J2/Navbar"
+import { Home } from "./components/J2/Home"
+import { Users } from "./components/J2/Users"
 import { Posts } from "./components/Posts"
 import { Post } from "./components/Post"
 
